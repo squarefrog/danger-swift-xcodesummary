bfa5a88d0d6a833f0f1e29571c9221249163a33f
Paul Williamson <squarefrog@gmail.com>
2022-08-08 12:28:21 +0100

    Use concurrent revision of danger swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift", from: "3.0.0"),
+        .package(url: "https://github.com/danger/swift", .revision("263436d79c51bf32e21b960f387bac4ef2d05c34")),
         .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev,
         .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.0.0"), // dev
     ],
