e38b1fb7e8b23c0f10a3b8ce95f1d782b5426e45
Paul Williamson <squarefrog@gmail.com>
2022-08-11 14:10:28 +0100

    Use master instead

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "DangerDepsSummary", type: .dynamic, targets: ["DangerDependencies"]) // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift", .revision("263436d79c51bf32e21b960f387bac4ef2d05c34")),
+        .package(url: "https://github.com/danger/swift", .branch("master")),
         .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev,
         .package(url: "https://github.com/squarefrog/danger-swift-coverage", .branch("concurrent")), // dev
     ],
